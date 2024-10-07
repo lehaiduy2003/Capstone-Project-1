2b80ac83a54c8608be56048f6dd3c88b50df36b3
Duy <lehaiduy2003@gmail.com>
2024-10-07 10:53:52 +0700

    build FE product detail

--- client/app/signUp.jsx
+++ client/app/signUp.jsx
@@ -13,7 +13,7 @@ import InputPass from "../components/InputPass";
 import { emailIsValid, passwordMatches } from "../utils/inputValidation";
 
 const SignUp = () => {
-  const api_url = process.env.EXPO_PUBLIC_API_URL + "/auth/sign-up";
+  const api_url = process.env.EXPO_PUBLIC_API_URL + "auth/sign-up";
   console.log(api_url);
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState(false);
