e5eb8eab23d326e19d91a8dd0bdbeb0441415ed2
lehaiduy2003 <lehaiduy2003@gmail.com>
2024-10-29 01:25:05 +0700

    fix sign in

---
+++ client/hooks/useAuthSubmit.js
@@ -0,0 +1,41 @@
+import {useState} from "react";
+import {Alert} from "react-native";
+import {fetchData} from "../utils/fetch";
+
+// for sending data via form to the server
+const useAuthSubmit = (apiUrl) => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
+    const onSubmit = async (options) => {
+        try {
+            setLoading(true);
+            // console.log(options.body);
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(options.body),
+            });
+            if (!response.ok) {
+                setError({code: response.status, message: response.body});
+                return null;
+            }
+            const jsonData = await response.json();
+            return jsonData;
+        } catch (error) {
+            Alert.alert("Form Submitting", "An error occurred. Please try again.");
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return {
+        loading,
+        error,
+        onSubmit,
+    };
+};
+
+export default useAuthSubmit;
--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,68 +1,69 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 
 import BaseController from "./init/BaseController";
 import AuthService from "../services/AuthService";
 
 export default class AuthController extends BaseController {
-  private readonly authService: AuthService;
+    private readonly authService: AuthService;
 
-  public constructor(authService: AuthService) {
-    super();
-    this.authService = authService;
-  }
-
-  /**
-   * for creating a new user
-   * @param req request body containing email, password, role
-   * @param res response containing the created user
-   */
-  public async signUp(req: Request, res: Response): Promise<void> {
-    if (!this.checkReqBody(req, res)) return;
-    try {
-      console.log("req.body", req.body);
-
-      const result = await this.authService.signUp(req.body);
-
-      if (!result) {
-        res.status(502).send({ error: "User already exists" });
-      } else res.status(201).send(result);
-    } catch (error) {
-      this.error(error, res);
+    public constructor(authService: AuthService) {
+        super();
+        this.authService = authService;
     }
-  }
 
-  /**
-   * Generate a new access token
-   * @param req request header containing refresh token
-   * @param res response containing the new access token
-   */
-  generateNewAccessToken(req: Request, res: Response): void {
-    try {
-      const newAccessToken = this.authService.getNewAccessToken(String(req.body.token));
-      res.status(201).send({ accessToken: newAccessToken });
-      return;
-    } catch (error) {
-      this.error(error, res);
+    /**
+     * for creating a new user
+     * @param req request body containing email, password, role
+     * @param res response containing the created user
+     */
+    public async signUp(req: Request, res: Response): Promise<void> {
+        if (!this.checkReqBody(req, res)) return;
+        try {
+            // console.log("req.body", req.body);
+            const result = await this.authService.signUp(req.body);
+            // console.log("result", result);
+            if (!result) {
+                res.status(502).send({error: "User already exists"});
+            } else res.status(201).send(result);
+        } catch (error) {
+            this.error(error, res);
+        }
     }
-  }
 
-  /**
-   * User sign in: check if the user exists and the password is correct
-   * @param req request body containing email and password
-   * @param res response containing the user info and tokens
-   */
-  async signIn(req: Request, res: Response): Promise<void> {
-    if (!this.checkReqBody(req, res)) return;
-    try {
-      const account = req.body;
-
-      const result = await this.authService.signIn(account.email, account.password);
-
-      if (!result) {
-        res.status(502).send({ error: "Invalid credential" });
-      } else res.status(200).send(result);
-    } catch (error) {
-      this.error(error, res);
+    /**
+     * Generate a new access token
+     * @param req request header containing refresh token
+     * @param res response containing the new access token
+     */
+    generateNewAccessToken(req: Request, res: Response): void {
+        try {
+            const newAccessToken = this.authService.getNewAccessToken(String(req.body.token));
+            res.status(201).send({accessToken: newAccessToken});
+            return;
+        } catch (error) {
+            this.error(error, res);
+        }
     }
-  }
+
+    /**
+     * User sign in: check if the user exists and the password is correct
+     * @param req request body containing email and password
+     * @param res response containing the user info and tokens
+     */
+    async signIn(req: Request, res: Response): Promise<void> {
+        if (!this.checkReqBody(req, res)) return;
+        try {
+            const account = req.body;
+
+            // console.log("account", account);
+
+            const result = await this.authService.signIn(account.email, account.password);
+            // console.log("result", result);
+            if (!result) {
+                res.status(502).send({error: "Invalid credential"});
+            } else res.status(200).send(result);
+        } catch (error) {
+            this.error(error, res);
+        }
+    }
 }
